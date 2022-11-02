756e8786bac5f0516444d9605df1e8eafae47526
tegan-rm <tegan.roberts.morgan@gmail.com>
2022-11-02 12:05:01 +0000

    Login page done

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var attempt = 3;
 function validate(){
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
@@ -7,12 +8,12 @@ function validate(){
     }
     else{
     attempt --;
-        alert("You have left "+attempt+" attempt;");
-    if( attempt == 0){
-        document.getElementById("username").disabled = true;
-        document.getElementById("password").disabled = true;
-        document.getElementById("submit").disabled = true;
-        return false;
-    }
+        if( attempt <= 0){
+            window.location = "fail.html";
+            return false;
+        }
+        else {
+            alert("You have "+attempt+" attempts left");
+        }  
     }
     }
