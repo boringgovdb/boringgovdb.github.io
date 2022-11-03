868284414d670a6a7c2c032088559099d847e526
JoanneLin168 <joannelin168@outlook.com>
2022-11-03 13:50:24 +0000

    Fixed audio

--- playAudios.js
+++ playAudios.js
@@ -1,5 +1,18 @@
-document.addEventListener('click', musicPlay);
-function musicPlay() {
-    document.getElementById('audio').play();
-    document.removeEventListener('click', musicPlay);
+function playAudio(src) {
+    var audioElement = new Audio(src);
+    audioElement.play();
+    return audioElement
+}
+  
+function playAudios(sequences) {
+    // play audio
+    let index = 0
+    const audioElement = playAudio(sequences[index])
+    audioElement.addEventListener('ended', (e) => {
+    index++
+    if (index < sequences.length) {
+        audioElement.src = sequences[index]
+        audioElement.play();
+    }
+    })
 }
